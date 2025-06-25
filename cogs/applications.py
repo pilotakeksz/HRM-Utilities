85a3f4f2b471a12ce62d6da250527523265b312c
pilotakeksz <matenyomtato@gmail.com>
2025-06-25 18:38:06 +0200

    e

--- cogs/applications.py
+++ cogs/applications.py
@@ -1,4 +1,3 @@
-
 import os
 import discord
 from discord.ext import commands
@@ -7,8 +6,12 @@ from discord.ui import View, Button
 
 APPLICATIONS_ROLE_ID = 1329910241835352064
 APPLICATIONS_CHANNEL_ID = int(os.getenv("APPLICATIONS_CHANNEL_ID", 1329910454059008101))
-APPLICATIONS_COLOR = int(os.getenv("APPLICATIONS_COLOR", "0xd0b47b"), 16)
+EMBED_COLOUR = int(os.getenv("EMBED_COLOUR", "0xd0b47b"), 16)
 TRAINER_PING_ROLE_ID = 1329910397544960164
+
+FOOTER_TEXT = "High Rock Military Corps"
+FOOTER_ICON = "https://images-ext-1.discordapp.net/external/_d7d0RmGwlFEwwKlYDfachyeC_skH7txYK5GzDan4ZI/https/cdn.discordapp.com/icons/1329908357812981882/fa763c9516fc5a9982b48c69c0a18e18.png"
+BOTTOM_IMAGE = "https://cdn.discordapp.com/attachments/1376647068092858509/1376648782359433316/bottom.png?ex=685cfbd6&is=685baa56&hm=8e024541f2cdf6bc41b83e1ab03f3da441b653dc98fa03f5c58aa2ccee0e3ad4&"
 
 class ApplyButtonView(View):
     def __init__(self):
@@ -58,40 +61,49 @@ class Applications(commands.Cog):
         ping_text = f"<@&{TRAINER_PING_ROLE_ID}>\n" if ping else ""
 
         # Embed 1 (image)
-        embed1 = discord.Embed(color=APPLICATIONS_COLOR)
+        embed1 = discord.Embed(color=EMBED_COLOUR)
         embed1.set_image(url="https://cdn.discordapp.com/attachments/1376647068092858509/1376933967386771546/applications.png?ex=685d5cb0&is=685c0b30&hm=1515149d32a78690a4e91e9d4879a9451c6f9fb0064d6aa651c256240a709ff5&")
 
         # Embed 2 (main info)
         embed2 = discord.Embed(
             description=(
                 "Welcome to the High Rock Military Application Hub! Bellow you will find the application, and more info on the Military Personnel position you may apply for here at HRM. We wish you the best of luck, and hope to see you on our team!\n\n"
-                "**Do you have a “blacklisted” role? Run `/verify` with @Bloxlink#6871 !**\n\n"
+                "**Do you have a “blacklisted” role? Run `/verify` with <@426537812993638400> !**\n\n"
                 "-# Bare in mind that any use of AI will result in a blacklist."
             ),
-            color=APPLICATIONS_COLOR
+            color=EMBED_COLOUR
         )
 
-        # Application Status field
+        # Application Status field (formatted as requested)
         if open:
             status = (
                 "> <:yes:1358812809558753401> **OPEN** <:yes:1358812809558753401>\n"
                 "> ⏰ **| Length: 15 Questions**\n"
-                "> 📑 **| Reading Time: 1-2 days**\n"
-                f"> **<:Member:1343945679390904330> | Trainer Availability: {trainer_availability.value}**"
+                f"> <:Member:1343945679390904330> **| Trainer Availability: {trainer_availability.value}**"
             )
         else:
             status = (
                 "> <:no:1358812780890947625> **CLOSED** <:no:1358812780890947625>\n"
                 "> ⏰ **| Length: 15 Questions**\n"
-                "> 📑 **| Reading Time: 1-2 days**\n"
-                f"> **<:Member:1343945679390904330> | Trainer Availability: {trainer_availability.value}**"
+                f"> <:Member:1343945679390904330> **| Trainer Availability: {trainer_availability.value}**"
             )
-        embed2.add_field(name="Application Status", value=status, inline=False)
 
-        view = ApplyButtonView()
-        await channel.send(content=ping_text if ping else None, embed=embed1)
-        await channel.send(embed=embed2, view=view)
-        await interaction.response.send_message("Application embed sent.", ephemeral=True)
+        embed2.add_field(
+            name="Application Status",
+            value=status,
+            inline=False
+        )
+        embed2.set_footer(text=FOOTER_TEXT, icon_url=FOOTER_ICON)
+        embed2.set_image(url=BOTTOM_IMAGE)
 
-async def setup(bot: commands.Bot):
+        # Send message
+        await channel.send(
+            content=ping_text,
+            embeds=[embed1, embed2],
+            view=ApplyButtonView()
+        )
+
+        await interaction.response.send_message("Application embed sent!", ephemeral=True)
+
+async def setup(bot):
     await bot.add_cog(Applications(bot))
