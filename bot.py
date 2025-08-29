274e05b6a28c1aad806adea9b87881e0b421a4bb
pilotakeksz <120522072+pilotakeksz@users.noreply.github.com>
2025-08-29 17:31:47 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -151,7 +151,8 @@ async def main():
             "cogs.embed",
             "cogs.review",
             "cogs.message",
-            "cogs.backups"
+            "cogs.backups",
+            "coge.ping"
         ]
         
         for cog in cogs:
